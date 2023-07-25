49b220e593131d2012c8f19739a11bd04c7645a5
luigy-lemon <luigy-lemon@protonmail.com>
2023-07-25 17:00:54 +0100

    foundry

---
+++ script/XDaiForeignBridge.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: gpl-2.0
+pragma solidity 0.4.24;
+
+//import 'forge-std/Script.sol';
+//import 'forge-std/console.sol';
+import 'src/upgradeable_contracts/erc20_to_native/XDaiForeignBridge.sol';
+
+contract XDaiForeignBridgeDeployer is Script {
+
+    function run() external {
+
+        /*//////////////////////////////////////////////////////////////
+                                KEY MANAGEMENT
+        //////////////////////////////////////////////////////////////*/
+
+        uint256 deployerPrivateKey = 0;
+        string memory mnemonic = vm.envString('MNEMONIC');
+
+        if (bytes(mnemonic).length > 30) {
+            deployerPrivateKey = vm.deriveKey(mnemonic, 0);
+        } else {
+            deployerPrivateKey = vm.envUint('PRIVATE_KEY');
+        }
+
+        vm.startBroadcast(deployerPrivateKey);
+        address deployer = vm.rememberKey(deployerPrivateKey);
+   //     console.log('Deployer: %s', deployer);
+
+        /*//////////////////////////////////////////////////////////////
+                                DEPLOYMENTS
+        //////////////////////////////////////////////////////////////*/
+
+        XDaiForeignBridge xdaiBridge = new XDaiForeignBridge();
+   //     console.log('Deployed XDaiForeignBridge: %s', address(xdaiBridge));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/foundry/interfaces/IEternalStorageProxy.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.10;
+
+interface Interface {
+    event ProxyOwnershipTransferred(address previousOwner, address newOwner);
+    event Upgraded(uint256 version, address indexed implementation);
+
+    function implementation() external view returns (address);
+    function proxyOwner() external view returns (address);
+    function transferProxyOwnership(address newOwner) external;
+    function upgradeTo(uint256 version, address implementation) external;
+    function upgradeToAndCall(uint256 version, address implementation, bytes memory data) external payable;
+    function upgradeabilityOwner() external view returns (address);
+    function version() external view returns (uint256);
+}
+
